c7b9c5408d81b883fd9a94f21f3d56fe1bb19916
getcontext <andrzej.salamon@gmail.com>
2018-09-09 20:49:13 +0200

    soc&websoc if, todo pull up

--- src/server/core/SocketConnection.java
+++ src/server/core/SocketConnection.java
@@ -3,8 +3,10 @@ package server.core;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * @todo pull up
+ */
 public interface SocketConnection extends Connection {
-
 	String getId();
     void handleStream();
     void handleStream(Socket client);
--- src/server/core/WebSocketConnection.java
+++ src/server/core/WebSocketConnection.java
@@ -5,20 +5,14 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by wizard on 6/11/17.
+ * @todo pull up
  */
 public interface WebSocketConnection extends SocketConnection {
     Integer MAX_BUFFER = 5000;
-
     void sendHandshake() throws NoSuchAlgorithmException, IOException;
-
     boolean isHandshake();
-
     boolean isHandshake(String data);
-
     boolean isGet();
-
     boolean isGet(String data);
-
     String getRequestAsString();
-
 }
