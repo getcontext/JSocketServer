03225ea29240bd891c9655daf1d3e9b4262cb05b
getcontext <andrzej.salamon@gmail.com>
2023-10-13 00:35:50 +0200

    fix of security S2755

--- src/server/config/ServerConfig.java
+++ src/server/config/ServerConfig.java
@@ -43,9 +43,9 @@ public class ServerConfig {
 
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-//            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-//
-//            factory.setFeature(XMLInputFactory.SUPPORT_DTD, false);
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+
+            factory.setFeature(XMLInputFactory.SUPPORT_DTD, false);
 
             DocumentBuilder docBuilder = factory.newDocumentBuilder();
             Document doc = docBuilder.parse(new File(file));
